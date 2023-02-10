aaf18985e75ca0b7e04f42a0428d2e79f1bb94d8
AndyMelR <ameleror@hotmail.es>
2023-02-10 15:15:44 +0000

    refactor v1.3

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
-window.onload = () => {
-  //write your code here
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#my_excuse").innerHTML = generateExcuse();
-  });
-};
 
-  function generateExcuse() {
-  const who = ["The dog", "My grandma", "His turtle", "My bird"];
-  const superWho = [{ id: "My dog", src: "https://giphy.com/embed/7792CoU4eXuoM" }, { id: "My turtle", src: "https://giphy.com/embed/j0RqBdOYVkzJJzJDoo" }, { id: "My grandma", src: "https://giphy.com/embed/jQAWv2kZe3hXA97PTu" }, { id: "My bird", src: "https://giphy.com/embed/X6Sm6CdSosJnJVa1tV" }];
-  const action = ["ate", "peed", "crushed", "broke"];
-  const what = ["my homework", "the keys", "the car"];
-  const when = [
-    "before the class !",
-    "right on time !",
-    "when I finished !",
-    "during my lunch !",
-    "while I was praying !"
-  ];
+function executeProgram() {
+  window.onload = () => {
+    //write your code here
+    document.querySelector("#btn").addEventListener("click", () => {
+      document.querySelector("#my_excuse").innerHTML = generateExcuse();
+    });
+  };
+}
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const superWho = [{ id: "My dog", src: "https://giphy.com/embed/7792CoU4eXuoM" }, { id: "My turtle", src: "https://giphy.com/embed/j0RqBdOYVkzJJzJDoo" }, { id: "My grandma", src: "https://giphy.com/embed/jQAWv2kZe3hXA97PTu" }, { id: "My bird", src: "https://giphy.com/embed/X6Sm6CdSosJnJVa1tV" }];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class !",
+  "right on time !",
+  "when I finished !",
+  "during my lunch !",
+  "while I was praying !"
+];
+
+function getArrayLength(array) {
+  let arrayLength = array.length;
+  return arrayLength;
+}
+
+function getRandomOption(excuseOptions) {
+  return Math.floor(Math.random() * getArrayLength(excuseOptions));
+}
+
+function generateExcuse(whoIndex, actionIndex, whatIndex, whenIndex) {
+  whoIndex = getRandomOption(who);
+  actionIndex = getRandomOption(action);
+  whatIndex = getRandomOption(what);
+  whenIndex = getRandomOption(when);
 
   document.getElementById("animation").src = superWho[whoIndex].src;
-
   return `${superWho[whoIndex].id}  ${action[actionIndex]}  ${what[whatIndex]}  ${when[whenIndex]}`;
-
 };
+
+executeProgram();
+
+
+
+
+
+
 
 
